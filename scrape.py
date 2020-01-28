84194161ba0562a79c66d4a256c353a5c2bb0cca
Jason <JasonMSchneider@outlook.com>
2020-01-28 11:19:23 -0500

    Added .gitignore for the credentials.csv file

--- scrape.py
+++ scrape.py
@@ -1,1 +1,14 @@
 #Initial Empty File
+import tweepy
+
+#You will need to import your own Twitter developer credentials
+#In order to do it, create a file called "credentials.csv"
+#And set it up as follows
+# consumer_key,consumer_secret,access_token,access_token_secret
+
+#This code will read in your credentials and then be able
+#to run
+
+
+
+auth = tweepy.OAuthHandler(
