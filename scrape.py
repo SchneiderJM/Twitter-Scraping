3dee17d8f9ab3fcd10142de7b9b99f558951f181
Jason Schneider <jasonmschneider@outlook.com>
2020-01-28 13:29:41 -0500

    Reads in Twitter credentials and sets up the file to interface with Twitter

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 #Initial Empty File
 import tweepy
-
+import pandas as pd
+import os
 #You will need to import your own Twitter developer credentials
 #In order to do it, create a file called "credentials.csv"
 #And set it up as follows
@@ -9,6 +10,14 @@ import tweepy
 #This code will read in your credentials and then be able
 #to run
 
+credentials = list(pd.read_csv(os.getcwd()+'\\credentials.csv').columns)
 
 
-auth = tweepy.OAuthHandler(
+auth = tweepy.OAuthHandler(credentials[0],credentials[1])
+auth.set_access_token(credentials[2],credentials[3])
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
